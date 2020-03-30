65085a1a7ea9ad419ebcccfa12289bf8662d391e
jie <jie@jiede-MacBook-Pro.local>
2020-03-31 00:04:05 +0800

    stable

--- test.php
+++ test.php
@@ -13,13 +13,16 @@ if (!$session) {
     return;
 }
 //24小時
-$times = [19, 20, 21];
+$times = [20, 21];
+
+$sections = [88, 87];
 
 date_default_timezone_set('Asia/Taipei');
 
 $get_ticket_date = date("Y/m/d", mktime(0, 0, 0, date("m"), date("d") + 8, date("Y")));
+//$get_ticket_date = '2020/04/02';
+$can_start_get_ticket_time = date("Y-m-d H:i:s", mktime(0, 0, 0, date("m"), date("d") + 1 , date("Y")));
 
-$can_start_get_ticket_time = date("Y-m-d H:i:s", mktime(0, 0, 0, date("m"), date("d") + 1, date("Y")));
 $end_get_ticket_time = date("Y-m-d H:i:s", mktime(0, 0, 10, date("m"), date("d") + 1, date("Y")));
 
 $is_can_get_ticket = true;
@@ -29,30 +32,34 @@ echo ('going to start loop' . PHP_EOL);
 while ($is_can_get_ticket) {
     $now = date("Y-m-d H:i:s", strtotime('now'));
 
-    if ($now > $can_start_get_ticket_time) {
+    if ($now >= $can_start_get_ticket_time) {
+        error_log($get_ticket_date. PHP_EOL, 3, "log.txt");
 
-        foreach ($times as $time) {
-            echo ("post time = {$time} " . PHP_EOL);
+        foreach ($sections as $section) {
+            foreach ($times as $time) {
+                echo ("post time = {$time} " . PHP_EOL);
 
-            postTicket($session, $get_ticket_date, $time);
+                postTicket($session, $get_ticket_date, $time, $section);
 
-            echo ("post time = {$time} end ". PHP_EOL );
+                echo ("post time = {$time} end " . PHP_EOL);
+            }
         }
+
     } else {
-        echo("time  not yet now = {$now}". PHP_EOL );
+        echo ("time  not yet now = {$now}" . PHP_EOL);
     }
 
     if ($now > $end_get_ticket_time) {
-        echo ('over time'. PHP_EOL );
+        echo ('over time' . PHP_EOL);
         $is_can_get_ticket = false;
-    }else{
-        echo("continue loop". PHP_EOL );
+    } else {
+        echo ("continue loop" . PHP_EOL);
     }
 
 }
-echo ('loop end'. PHP_EOL );
+echo ('loop end' . PHP_EOL);
 
-function postTicket($session, $get_ticket_date, $order_time)
+function postTicket($session, $get_ticket_date, $order_time, $section)
 {
     if (!$get_ticket_date || !$order_time) {
         return;
@@ -69,7 +76,7 @@ function postTicket($session, $get_ticket_date, $order_time)
         'module' => 'net_booking',
         'files' => 'booking_place',
         'StepFlag' => 25,
-        'QPid' => 84,
+        'QPid' => $section,
         'QTime' => $order_time,
         'D' => $get_ticket_date,
     ];
